fbfce9a2d79b8feb24f2b22f0f7d24582a8c42b5
Vishesh Singh <VisheshSingh16@gmail.com>
2020-02-01 09:47:52 -0600

    forEach HOF

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ const doubleBtn = document.getElementById('double-money');
 const showMillionaireBtn = document.getElementById('show-millionaire');
 const sortBtn = document.getElementById('sort');
 const calculateSumBtn = document.getElementById('calculate-sum');
+const main = document.getElementById('main');
 
 let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+getRandomUser();
 
 async function getRandomUser() {
   const res = await fetch('https://randomuser.me/api');
@@ -22,4 +28,27 @@ async function getRandomUser() {
 
 function addData(obj) {
   data.push(obj);
+
+  updateDOM();
 }
+
+function updateDOM(providedData = data) {
+  // clear main element
+  main.innerHTML = `<h2><strong>Person</strong> Wealth</h2>`;
+
+  providedData.forEach(item => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong> ${formatCurrency(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+}
+
+function formatCurrency(number) {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+// addUser
+addUserBtn.addEventListener('click', getRandomUser);
